33a9818aeb30fc2c81fab07a5f2bee4130fac1de
Thiago Santos <91476713+Thmsantos@users.noreply.github.com>
2024-03-11 11:52:17 -0300

    Delete PI_fatec -- input students directory

--- PI_fatec -- input students/Client/InputS_login/code.js
+++
@@ -1,9 +0,0 @@
-function showAlert() {
-    var customAlert = document.getElementById('customAlert');
-    customAlert.style.display = 'block';
-}
-
-function closeAlert() {
-    var customAlert = document.getElementById('customAlert');
-    customAlert.style.display = 'none';
-}
--- PI_fatec -- input students/Client/README.md
+++
@@ -1,4 +0,0 @@
-# Input Students
-<p>Repositório do Projeto Integrador</p>
-<p>O Input Students é um software que será desenvolvido para a divulgação do curso de DSM da Fatec Diadema e integração dos novos alunos.</p> 
-
