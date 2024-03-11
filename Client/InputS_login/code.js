e1b0f98758fd7978d4f3ddf75e85ebf3731246af
Thiago Santos <91476713+Thmsantos@users.noreply.github.com>
2024-03-11 11:56:22 -0300

    Add files via upload

---
+++ Client/InputS_login/code.js
@@ -0,0 +1,9 @@
+function showAlert() {
+    var customAlert = document.getElementById('customAlert');
+    customAlert.style.display = 'block';
+}
+
+function closeAlert() {
+    var customAlert = document.getElementById('customAlert');
+    customAlert.style.display = 'none';
+}
